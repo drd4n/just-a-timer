15ceb45c211f72a8990da86ff57db29d3a0e8c6b
drd4n <the_dann@hotmail.co.th>
2019-05-11 18:47:54 +0700

    add time but just in second

---
+++ src/component/Timer.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+class Timer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0
+        }
+    }
+
+    render() {
+        const { count } = this.state
+        return (
+            <div>
+
+                <p>Time Count = {count}</p>
+                <input type="number" id='tnum'></input>
+                <button type="button" onclick=''>Snap!</button>
+            </div>
+
+        )
+    }
+
+    componentDidMount() {
+        const {time} = this.props
+        this.setState({
+            count: time
+        })
+        this.doIntervalChange()
+
+    }
+
+    doIntervalChange =()=>{
+        this.myInterval = setInterval(() => {
+            this.setState(prevState => ({
+
+                count: prevState.count - 1
+            }))
+        }, 1000)
+    }
+
+
+
+    componentWillUnmount() {
+        clearInterval(this.myInterval)
+    }
+}
+
+export default Timer;
